5a7fa002cf4af1a980c0bd4bff8c99fba294b4e1
thiago <thiago.lopes.dev@gmail.com>
2020-11-25 16:23:31 -0300

    feat: add support to cors and routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import Routes from './routes/routes';
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json())
+app.use(cors()) 
+app.use(Routes)
+
 
 app.listen(PORT, () => console.log(`App is running at port ${PORT}`));
