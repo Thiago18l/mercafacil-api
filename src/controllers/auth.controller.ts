4180560462264ffdafe761bb16e07bbe550886af
thiago <thiago.lopes.dev@gmail.com>
2020-11-26 10:40:53 -0300

    feat: add logout function and interfaces

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken';
 import { pg_pool } from '../database';
 import { QueryResult } from 'pg';
 
-
 interface IBody {
     name: string;
     password: string;
@@ -20,19 +19,40 @@ interface IContacts {
     cellphone: string;
 }
 
-export const getUser = async (req: Request, res: Response): Promise<Response> => {
+interface IFormUser {
+    name: string;
+    password: string;
+}
+
+
+export const createUser = async (req: Request, res: Response): Promise<Response> => {
     try {
-        const response: QueryResult = await pg_pool.query("SELECT id, nome FROM contacts")
-        return res.status(200).json(response.rows)
+        const { name, password }: IFormUser = req.body;
+        const response: QueryResult = await pg_pool.query(
+            `INSERT INTO users (name, password) values ('${name}', '${password}')`
+        );
+        console.log(response.rows);
+        return res.status(201).send({ message: "user created" });
     } catch (e) {
         return res.status(500).json("Error")
+    }
+}
+
+export const getUsers = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const response: QueryResult = await pg_pool.query(`SELECT * FROM users`);
+        return res.status(200).send(response.rows);
+    } catch (e) {
+        return res.status(401).send({ message: e })
     }
 }
 
 export const loginUser = async (req: Request, res: Response): Promise<Response> => {
     try {
         const { name, password }: IBody = req.body;
-        const response: QueryResult = await pg_pool.query("SELECT id, name, password FROM users");
+        const response: QueryResult = await pg_pool.query(`
+            SELECT id, name, password FROM users WHERE name = '${name}' and password = '${password}'`
+        );
         const data: IData[] = response.rows;
         if (name === data[0].name && password === data[0].password) {
             const token = jwt.sign({ id: data[0].id }, 'IMAGINARYTOKEN', { expiresIn: 300 });
@@ -40,7 +60,7 @@ export const loginUser = async (req: Request, res: Response): Promise<Response> 
         }
         return res.status(500).send("Login inválido!")
     } catch (e) {
-        return res.status(401).json(`Error ${e}`);
+        return res.status(401).json(`Login inválido`);
     }
 }
 
@@ -48,13 +68,23 @@ export const loginUser = async (req: Request, res: Response): Promise<Response> 
 export const createListOfContacts = async (req: Request, res: Response): Promise<Response> => {
     try {
         const { name, cellphone }: IContacts = req.body;
-        console.log(name, cellphone);
         const result: QueryResult = await pg_pool.query(
             `INSERT INTO contacts (nome, celular) VALUES ('${name}', '${cellphone}')`
         );
-        console.log(result.rows)
         return res.status(201).send({ message: "Contact created"});
     } catch (e) {
         return res.status(500).send(`${e}`);
+    }
+}
+
+
+/**
+ * Logout
+ */
+export const logout =  (req: Request, res: Response) => {
+    try {
+        return res.status(200).send({ auth: false, token: null })
+    } catch (e) {
+        return res.status(500).end();
     }
 }
