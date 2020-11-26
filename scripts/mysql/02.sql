9b68f89721a2c8b1c9406b291475c5554ba76357
thiago <thiago.lopes.dev@gmail.com>
2020-11-26 10:41:35 -0300

    chore: add scripts to initialize tables

--- scripts/mysql/02.sql
+++ scripts/mysql/02.sql
@@ -4,4 +4,11 @@ CREATE table contacts (
 	id serial PRIMARY KEY,
 	nome VARCHAR ( 200 ) NOT NULL,
 	celular VARCHAR ( 20 ) NOT NULL
-);  
+);
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(40) NOT NULL,
+  password TEXT NOT NULL
+);
+
+INSERT INTO users (name, password) values ('thiago', '123');
--- scripts/postgres/01.sql
+++ scripts/postgres/01.sql
@@ -4,3 +4,11 @@ CREATE table contacts (
 	nome VARCHAR ( 100 ) NOT NULL,
 	celular VARCHAR ( 13 ) NOT NULL
 );
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(40) NOT NULL,
+  password TEXT NOT NULL
+);
+
+INSERT INTO users (name, password) values ('thiago', '123');
