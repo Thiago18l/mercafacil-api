e728940b52655f4894c97ddb058df1c37e3fe1f1
thiago <thiago.lopes.dev@gmail.com>
2020-11-26 00:10:03 -0300

    feat: add dotenv load

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import Routes from './routes/routes';
+import dotenv from 'dotenv';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+dotenv.config();
 
 app.use(bodyParser.json())
 app.use(cors()) 
