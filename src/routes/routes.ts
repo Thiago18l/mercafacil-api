6b9e9a7f4583110dbcbddc2028aee5abf90fa867
thiago <thiago.lopes.dev@gmail.com>
2020-11-26 00:12:35 -0300

    feat: add routes for validation of tokens

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,13 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
+import { getUser, loginUser, createListOfContacts } from '../controllers';
 
+
+import { TokenValidation } from '../auth';
 const routes = Router();
 
-/**
- * rota default da aplicação
- */
-routes.get("/", (req: Request, res: Response) => {
-    res.json({'message': 'mercafacil api'});
-});
-
+routes.get('/user', getUser)
+routes.post('/login', loginUser)
+routes.get('/clientes', TokenValidation, createListOfContacts)
+routes.get('/logout', )
 
 export default routes;
