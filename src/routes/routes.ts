10c9cf8f3985b7bea93981f66452f3d60e704f78
thiago <thiago.lopes.dev@gmail.com>
2020-11-26 10:43:22 -0300

    feat: add routes for mysql db

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,26 @@
 import { Router } from 'express';
-import { getUser, loginUser, createListOfContacts } from '../controllers';
-
-
+import { createUser, getUsers, loginUser, createListOfContacts, logout} from '../controllers';
+import { create_User, login_User, get_Users, create_contacts} from '../controllers';
 import { TokenValidation } from '../auth';
 const routes = Router();
 
-routes.get('/user', getUser)
-routes.post('/login', loginUser)
-routes.get('/clientes', TokenValidation, createListOfContacts)
-routes.get('/logout', )
+
+/**
+ * Rotas para o varejão
+ */
+routes.get('/user/varejao', getUsers);
+routes.post('/user/varejao', createUser);
+routes.post('/login/varejao', loginUser);
+routes.post('/clientes/varejao', TokenValidation, createListOfContacts);
+routes.get('/logout/varejao', logout);
+
+/**
+ * Rotas para o macapa
+ */
+routes.get('/user/macapa', get_Users);
+routes.post('/user/macapa', create_User);
+routes.post('/macapa/login', login_User);
+routes.post('/macapa/clientes', TokenValidation, create_contacts);
+routes.get('/macapa/logout', logout);
 
 export default routes;
